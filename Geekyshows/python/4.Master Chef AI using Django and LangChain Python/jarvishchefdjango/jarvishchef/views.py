338e3b7f22ab15d587907c05ebada3bfbcc4ac49
Satyam Seth <satyam1998.1998@gmail.com>
2025-03-31 00:20:19 +0530

    Add form and css with home page

--- Geekyshows/python/4.Master Chef AI using Django and LangChain Python/jarvishchefdjango/jarvishchef/views.py
+++ Geekyshows/python/4.Master Chef AI using Django and LangChain Python/jarvishchefdjango/jarvishchef/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from jarvishchef.forms import RecipeForm
 
 # Create your views here.
+
+
+class Home(View):
+    def get(self, request):
+        form = RecipeForm()
+        return render(request, "jarvishchef/home.html", {"form": form})
+
+    def post(self, request):
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            recipe_message = form.cleaned_data["recipe_message"]
+            print(recipe_message)
+        form = RecipeForm()
+        return redirect("/")
